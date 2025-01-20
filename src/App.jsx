0ac6913f690bd5d3bd833633a122cfbe8daf371e
Erfan79Eftekhari <erfaneftekhari0079@gmail.com>
2025-01-20 15:07:26 +0330

    Adjust routing problem.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Notfound from "./pages/notFound/notFound.jsx";
 const App = () => {
   return (
     <Routes>
-      <Route path="/payment" element={<PaymentPage />} />
+      <Route path="/" element={<PaymentPage />} />
       <Route path="/*" element={<Notfound />} />
     </Routes>
   );
