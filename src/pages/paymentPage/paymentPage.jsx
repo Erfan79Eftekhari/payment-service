e0f1bd9a2dc16a95bf7e11860e3a7c930b9079c7
Erfan79Eftekhari <erfaneftekhari0079@gmail.com>
2025-01-30 17:17:36 +0330

    testing if i can send something back, okay?

--- src/pages/paymentPage/paymentPage.jsx
+++ src/pages/paymentPage/paymentPage.jsx
@@ -8,7 +8,14 @@ function PaymentPage() {
       console.error("Error calling Android payment function:", error);
     }
   };
-
+  const handleHello = () => {
+    try {
+      // window.Android.DoPayment(3000);
+      window.Android.DoHello(3000);
+    } catch (error) {
+      console.error("Error calling Android Hello function:", error);
+    }
+  };
   return (
     <div
       style={{
@@ -31,6 +38,20 @@ function PaymentPage() {
         }}
       >
         Make Payment
+      </button>
+      <button
+        onClick={handleHello}
+        style={{
+          padding: "12px 24px",
+          fontSize: "16px",
+          backgroundColor: "#007bff",
+          color: "white",
+          border: "none",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }}
+      >
+        Say Hello
       </button>
     </div>
   );
