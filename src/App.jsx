a8674f294925d153b1b34ae9993770deff61f4a4
Erfan79Eftekhari <erfaneftekhari0079@gmail.com>
2025-02-12 11:20:26 +0330

    still working on print with verification. minor name file bug fix.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import PaymentPage from "./pages/paymentPage/paymentPage.jsx";
 import Notfound from "./pages/notFound/notFound.jsx";
 import PrePayment from "./pages/prePayment/prePayment.jsx";
-import print from "./pages/print/print.jsx";
+import Print from "./pages/print/print.jsx";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<PaymentPage />} />
       <Route path="/prepayment" element={<PrePayment />} />
-      <Route path="/print" element={<print />} />
+      <Route path="/print" element={<Print />} />
       <Route path="/*" element={<Notfound />} />
     </Routes>
   );
